7209339eaa8a1419abad766f0b067fee90783276
hendraaagil <hendraaagil@gmail.com>
2025-02-10 20:28:22 +0700

    feat: implement invoice addition page with form and layout

--- src/app/invoices/add/page.tsx
+++ src/app/invoices/add/page.tsx
@@ -1,3 +1,25 @@
+import { AddForm } from '@/components/invoices'
+import { Container, Divider, Paper, Typography } from '@mui/material'
+
 export default function AddPage() {
-  return <div>Add</div>
+  return (
+    <Container sx={{ py: '32px' }}>
+      <Typography
+        variant="h4"
+        fontWeight={700}
+        sx={{ mb: '32px' }}
+        gutterBottom
+      >
+        Add Invoice
+      </Typography>
+
+      <Paper sx={{ py: '16px' }} elevation={16}>
+        <Typography fontWeight={700} sx={{ px: '24px' }}>
+          Invoice Form
+        </Typography>
+        <Divider sx={{ my: '16px' }} />
+        <AddForm />
+      </Paper>
+    </Container>
+  )
 }
