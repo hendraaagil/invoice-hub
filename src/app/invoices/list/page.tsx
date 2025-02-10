0f85be1014e4f6af98cb354b60c6126f1b85db00
hendraaagil <hendraaagil@gmail.com>
2025-02-10 23:04:11 +0700

    feat: fetch invoices from zustand store

--- src/app/invoices/list/page.tsx
+++ src/app/invoices/list/page.tsx
@@ -1,3 +1,10 @@
+'use client'
+
+import { useInvoiceStore } from '@/stores/invoice'
+
 export default function ListPage() {
+  const invoices = useInvoiceStore((state) => state.invoices)
+
+  console.log(invoices)
   return <div>List</div>
 }
