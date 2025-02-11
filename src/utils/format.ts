c33d4dd29574ddae29f6a99a1d7798b23b6f6f2b
hendraaagil <hendraaagil@gmail.com>
2025-02-12 06:13:25 +0700

    feat: update currency formatting

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -8,5 +8,6 @@ export const formatCurrency = (amount: string) => {
   return new Intl.NumberFormat('id-ID', {
     style: 'currency',
     currency: 'IDR',
+    minimumFractionDigits: 0,
   }).format(parseInt(amount))
 }
