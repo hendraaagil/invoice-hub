8362c0e90e52388d9a0bdf156cc6ed644fb0ad8b
hendraaagil <hendraaagil@gmail.com>
2025-02-12 22:04:51 +0700

    feat: enhance loading, not found, and action on list

--- src/components/invoices/list.tsx
+++ src/components/invoices/list.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import type { PropsWithChildren } from 'react'
+import { useState, type PropsWithChildren } from 'react'
 import { Menu } from '@mui/icons-material'
 import {
   Chip,
@@ -11,42 +11,81 @@ import {
   TableHead,
   TableRow,
   Typography,
-  Box,
   Skeleton,
 } from '@mui/material'
 
+import { Action } from './action'
+import { Invoice } from '@/lib/types/invoice'
 import { formatCurrency, formatDate } from '@/utils/format'
 import { useInvoiceStore } from '@/stores/invoice'
 import { useDebounce } from '@/hooks/use-debounce'
 import { useInvoiceFilters } from '@/hooks/use-invoice-filters'
 import { useFilteredInvoices } from '@/hooks/use-filtered-invoices'
+import { usePopoverAction } from '@/hooks/use-popover-action'
 
-function TableWrapper({ children }: PropsWithChildren) {
-  return (
-    <Table>
-      <TableHead
-        sx={{
-          backgroundColor: (theme) =>
-            theme.palette.mode === 'light' ? 'grey.100' : 'grey.800',
-        }}
-      >
-        <TableRow>
-          {['Invoice', 'Due Date', 'Status', 'Amount', 'Actions'].map(
-            (header) => (
-              <TableCell key={header}>{header}</TableCell>
-            ),
-          )}
-        </TableRow>
-      </TableHead>
-      <TableBody>{children}</TableBody>
-    </Table>
-  )
-}
+const TableWrapper = ({ children }: PropsWithChildren) => (
+  <Table>
+    <TableHead
+      sx={{
+        backgroundColor: (theme) =>
+          theme.palette.mode === 'light' ? 'grey.100' : 'grey.800',
+      }}
+    >
+      <TableRow>
+        {['Invoice', 'Due Date', 'Status', 'Amount', 'Actions'].map(
+          (header) => (
+            <TableCell key={header}>{header}</TableCell>
+          ),
+        )}
+      </TableRow>
+    </TableHead>
+    <TableBody>{children}</TableBody>
+  </Table>
+)
+
+const TableSkeleton = () => (
+  <TableWrapper>
+    {[...Array(5)].map((_, index) => (
+      <TableRow key={index}>
+        <TableCell>
+          <Skeleton width={150} height={24} />
+          <Skeleton width={100} height={20} />
+        </TableCell>
+        <TableCell>
+          <Skeleton width={100} height={24} />
+        </TableCell>
+        <TableCell>
+          <Skeleton width={80} height={32} />
+        </TableCell>
+        <TableCell>
+          <Skeleton width={90} height={24} />
+        </TableCell>
+        <TableCell>
+          <Skeleton width={40} height={40} variant="circular" />
+        </TableCell>
+      </TableRow>
+    ))}
+  </TableWrapper>
+)
+
+const TableNotFound = () => (
+  <TableWrapper>
+    <TableRow>
+      <TableCell colSpan={5}>
+        <Typography align="center" color="textSecondary">
+          No invoices found
+        </Typography>
+      </TableCell>
+    </TableRow>
+  </TableWrapper>
+)
 
 export function List() {
-  const { invoices } = useInvoiceStore((state) => state)
+  const [selectedInvoice, setSelectedInvoice] = useState<Invoice | null>(null)
+  const invoices = useInvoiceStore((state) => state.invoices)
   const { query, status } = useInvoiceFilters()
   const debouncedQuery = useDebounce(query)
+  const popover = usePopoverAction()
 
   const { filteredInvoices, isLoading } = useFilteredInvoices(
     invoices,
@@ -55,40 +94,10 @@ export function List() {
   )
 
   if (isLoading) {
-    return (
-      <TableWrapper>
-        {[...Array(5)].map((_, index) => (
-          <TableRow key={index}>
-            <TableCell>
-              <Skeleton width={150} height={24} />
-              <Skeleton width={100} height={20} />
-            </TableCell>
-            <TableCell>
-              <Skeleton width={100} height={24} />
-            </TableCell>
-            <TableCell>
-              <Skeleton width={80} height={32} />
-            </TableCell>
-            <TableCell>
-              <Skeleton width={90} height={24} />
-            </TableCell>
-            <TableCell>
-              <Skeleton width={40} height={40} variant="circular" />
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableWrapper>
-    )
+    return <TableSkeleton />
   }
-
   if (filteredInvoices.length === 0) {
-    return (
-      <Box p={3} textAlign="center">
-        <Typography color="textSecondary">
-          No invoices found matching your criteria
-        </Typography>
-      </Box>
-    )
+    return <TableNotFound />
   }
 
   return (
@@ -117,12 +126,26 @@ export function List() {
           </TableCell>
           <TableCell>{formatCurrency(invoice.amount)}</TableCell>
           <TableCell>
-            <IconButton aria-label="Actions">
+            <IconButton
+              aria-label="Actions"
+              aria-describedby="popover-actions"
+              onClick={(event) => {
+                popover.onOpen(event)
+                setSelectedInvoice(invoice)
+              }}
+            >
               <Menu />
             </IconButton>
           </TableCell>
         </TableRow>
       ))}
+      <Action
+        id="popover-actions"
+        anchorEl={popover.anchorEl}
+        open={popover.open}
+        onClose={popover.onClose}
+        invoice={selectedInvoice as Invoice}
+      />
     </TableWrapper>
   )
 }
