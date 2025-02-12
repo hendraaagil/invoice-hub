bfb81093e16a575c4f07a4433727918c59ee8c56
hendraaagil <hendraaagil@gmail.com>
2025-02-12 22:04:22 +0700

    feat: add delete action modal for invoices

---
+++ src/components/invoices/action.tsx
@@ -0,0 +1,127 @@
+import { useState } from 'react'
+import {
+  Backdrop,
+  Box,
+  Button,
+  Fade,
+  Modal,
+  ModalProps,
+  Popover,
+  PopoverProps,
+  Typography,
+} from '@mui/material'
+
+import { Invoice } from '@/lib/types/invoice'
+import { useInvoiceStore } from '@/stores/invoice'
+
+function ModalDelete({
+  open,
+  onClose,
+  invoice,
+}: {
+  onClose: () => void
+  invoice: Invoice
+} & Omit<ModalProps, 'children'>) {
+  const deleteInvoice = useInvoiceStore((state) => state.deleteInvoice)
+
+  return (
+    <Modal
+      aria-labelledby="modal-delete-title"
+      aria-describedby="modal-delete-description"
+      open={open}
+      onClose={onClose}
+      closeAfterTransition
+      slots={{ backdrop: Backdrop }}
+      slotProps={{
+        backdrop: {
+          timeout: 500,
+        },
+      }}
+    >
+      <Fade in={open}>
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 400,
+            bgcolor: 'background.paper',
+            boxShadow: 24,
+            p: 4,
+            borderRadius: 2,
+          }}
+        >
+          <Typography id="modal-delete-title" variant="h6" component="h2">
+            Are you sure?
+          </Typography>
+          <Typography id="modal-delete-description" sx={{ mt: 2 }}>
+            This action cannot be undone
+          </Typography>
+          <Box sx={{ mt: 4, display: 'flex', justifyContent: 'flex-end' }}>
+            <Button variant="outlined" color="primary" onClick={onClose}>
+              Cancel
+            </Button>
+            <Button
+              variant="contained"
+              color="error"
+              sx={{ ml: 2 }}
+              onClick={() => {
+                onClose()
+                deleteInvoice(invoice.id)
+              }}
+              disableElevation
+            >
+              Delete
+            </Button>
+          </Box>
+        </Box>
+      </Fade>
+    </Modal>
+  )
+}
+
+export function Action({
+  invoice,
+  ...props
+}: {
+  invoice: Invoice
+} & PopoverProps) {
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => {
+    setOpen(true)
+  }
+  const handleClose = () => {
+    props.onClose?.({}, 'backdropClick')
+    setOpen(false)
+  }
+
+  return (
+    <>
+      <ModalDelete open={open} onClose={handleClose} invoice={invoice} />
+      <Popover
+        {...props}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+      >
+        <Box p={1.5} sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+          <Button
+            color="primary"
+            variant="contained"
+            sx={{ textTransform: 'capitalize' }}
+            disableElevation
+          >
+            Edit
+          </Button>
+          <Button
+            color="error"
+            variant="outlined"
+            sx={{ textTransform: 'capitalize' }}
+            onClick={handleOpen}
+          >
+            Delete
+          </Button>
+        </Box>
+      </Popover>
+    </>
+  )
+}
